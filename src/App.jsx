e59865e93da9ce3f5f501bce1bb3fbb74786b960
Shaheer Ahmed <128972759+SherryMaster@users.noreply.github.com>
2025-07-20 10:20:23 +0500

    Initialize project with React, TailwindCSS, and Appwrite integration. Add essential files including App component, eslint configuration, styles, and API utilities.

---
+++ src/App.jsx
@@ -0,0 +1,139 @@
+import Search from "./components/Search.jsx";
+import {useEffect, useState} from "react";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
+import {useDebounce} from "react-use";
+import {getTrendingMovies, updateSearchTerm} from "./appwrite.js";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState("");
+  const [trendingLoading, setTrendingLoading] = useState(true);
+  const [trendingError, setTrendingError] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  const [trendingMovies, setTrendingMovies] = useState([])
+
+  useDebounce(
+    () => {
+      setDebouncedSearchTerm(searchTerm);
+    },
+    500,
+    [searchTerm]
+  )
+
+  const fetchMovies = async (query = "") =>{
+    setLoading(true);
+    setError("");
+    try {
+      const endpoint = query ?
+        `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` :
+        `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+
+      if (data.Response === "False"){
+        setError(data.Error || "Error Fetching Movies, Try again Later!");
+        setMovies([]);
+      }
+      setMovies(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchTerm(query, data.results[0]);
+      }
+    } catch (e) {
+      console.log(`Error Fetching movies: ${e}`);
+      setError("Error Fetching Movies, Try again Later!")
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try{
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+      setTrendingLoading(false);
+    } catch (e) {
+      console.log(`Error Fetching trending movies: ${e}`);
+      setTrendingError("Error Fetching Trending Movies, Try again Later!");
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm).then(r => r);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
+  return (
+    <>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="hero-img.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You Will Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="trending">
+          <h2>Trending Searches</h2>
+          { trendingLoading ? (
+            <Spinner />
+          ) : trendingError ? (
+            <p className="text-red-500">{trendingError}</p>
+          ) :
+            trendingMovies.length > 0 && (
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.posterUrl} alt={movie.searchTerm} />
+                </li>
+              ))}
+            </ul>
+            )
+          }
+        </section>
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {loading ? (
+            <Spinner />
+          ) : error ? (
+            <p className="text-red-500">{error}</p>
+          ) : (
+            <ul>
+              {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )
+          }
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const MovieCard = ({movie:
+  {title, poster_path, release_date, vote_average, vote_count, original_language}
+}) => {
+  return (
+    <div className="movie-card">
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : "/No-Poster-Portrait.png"} alt={title} />
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="content">
+          <p className="rating">
+            <img src="Star.svg" alt="Star" />
+            <span>{vote_average ? vote_average.toFixed(1) : "N/A"} ({vote_count ? vote_count : "N/A"} votes)</span>
+          </p>
+          <span>•</span>
+          <p className="lang">{original_language}</p>
+          <span>•</span>
+          <p className="year">{release_date ? new Date(release_date).getFullYear() : "N/A"}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default MovieCard
